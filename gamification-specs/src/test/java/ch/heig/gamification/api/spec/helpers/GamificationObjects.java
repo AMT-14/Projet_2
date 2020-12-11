33885220003917c1f69fefd084707965a8244a18
Florian Mülhauser <florian.mulhauser@heig-vd.ch>
2020-12-11 10:10:46 +0100

    update the specs, create an helper class

---
+++ gamification-specs/src/test/java/ch/heig/gamification/api/spec/helpers/GamificationObjects.java
@@ -0,0 +1,36 @@
+package ch.heig.gamification.api.spec.helpers;
+
+import ch.heig.gamification.api.dto.*;
+import lombok.Getter;
+import lombok.Setter;
+
+public class GamificationObjects {
+
+    @Getter
+    @Setter
+    static Application application;
+
+    @Getter
+    @Setter
+    User user;
+
+    @Getter
+    @Setter
+    Event event;
+
+    @Getter
+    @Setter
+    ScoreScale scorescale;
+
+    @Getter
+    @Setter
+    Badge badge;
+
+    @Getter
+    @Setter
+    Rule rule;
+
+    //TODO add last created Object for each ?
+
+
+}

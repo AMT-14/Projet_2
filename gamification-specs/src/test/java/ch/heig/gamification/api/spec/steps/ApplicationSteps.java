266d80b301ad3d6f4c9f53d2c122caebd7e47a49
Florian Mülhauser <florian.mulhauser@heig-vd.ch>
2020-12-11 13:23:55 +0100

    refactor ApllicationSteps with new common code

--- gamification-specs/src/test/java/ch/heig/gamification/api/spec/steps/ApplicationSteps.java
+++ gamification-specs/src/test/java/ch/heig/gamification/api/spec/steps/ApplicationSteps.java
@@ -5,6 +5,7 @@ import ch.heig.gamification.ApiException;
 import ch.heig.gamification.ApiResponse;
 import ch.heig.gamification.api.DefaultApi;
 import ch.heig.gamification.api.dto.Application;
+import ch.heig.gamification.api.spec.helpers.GamificationObjects;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
@@ -18,29 +19,16 @@ import static org.junit.Assert.*;
 
 public class ApplicationSteps {
 
-//    public class ApplicationSummary{
-//        private Application application;
+    private DefaultApi api;
+    private Environment env;
+    private GamificationObjects main;
+
 //
-//
-//    }
-    private final DefaultApi api = new DefaultApi();
-    private Application application;
-    private List<Application> applications;
-    //
-//    Application application;
-//
-//    private ApiResponse lastApiResponse;
-//    private ApiException lastApiException;
-//    private boolean lastApiCallThrewException;
-    private int statusCode;
-//
-//    private String lastReceivedLocationHeader;
-//    private Application lastReceivedApplication;
-//
-//    public ApplicationSteps(Environment environment) {
-//        this.environment = environment;
-//        this.api = environment.getApi();
-//    }
+    public ApplicationSteps(GamificationObjects main,Environment env) {
+        this.env = env;
+        this.api = env.getApi();
+        this.main = main;
+    }
 
     @Given("there is an Application server")
     public void there_is_an_Application_server() throws Throwable {
@@ -49,20 +37,27 @@ public class ApplicationSteps {
 
     @Given("^I have an application payload$")
     public void i_have_an_application_payload() throws Throwable {
-        application = new Application();
+        Application application = new Application();
         application.setName("Application42");
+        main.setApplication(application);
     }
 
     @When("^I POST it to the /applications endpoint$")
     public void i_POST_it_to_the_applications_endpoint() throws Throwable {
-        api.registerApplication(application);
-        ApiResponse response = api.registerApplicationWithHttpInfo(application);
-        statusCode = response.getStatusCode();
+        try {
+            ApiResponse response = api.registerApplicationWithHttpInfo(main.getApplication());
+            env.setApiResponse(response);
+            // now process
+            env.ApiResponseProcessor(env.getApiResponse());
+            GamificationObjects.setApplication((Application) env.getApiResponse().getData());
+        } catch (ApiException ex){
+            env.ApiExceptionProcessor(ex);
+        }
     }
 
     @Then("^I receive a (\\d+) status code$")
     public void i_receive_a_status_code(int expectedStatusCode) throws Throwable {
-        assertEquals(expectedStatusCode, statusCode);
+        assertEquals(expectedStatusCode, env.getHTTPStatus());
     }
 
 //    @When("^I ask for a list of registered applications with a GET on the /registrations endpoint$")
@@ -73,9 +68,9 @@ public class ApplicationSteps {
     @Then("^I see my application in the list$")
     public void i_see_my_application_in_the_list() throws Throwable {
         Application expected = new Application();
-        expected.setName(application.getName());
+        expected.setName(main.getApplication().getName());
 
-        assert(applications).contains("Application42");
+        assert(expected.getName()).equals("Application42");
 
     }
 //

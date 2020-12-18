074c2907f7ba061a4b846bb35abfa098cfaf022d
Basile <basile.botebol@gmail.com>
2020-12-18 12:04:58 +0100

    working spec for userStat

--- gamification-impl/src/main/java/ch/heig/gamification/repositories/RewardRepository.java
+++ gamification-impl/src/main/java/ch/heig/gamification/repositories/RewardRepository.java
@@ -5,4 +5,5 @@ import ch.heig.gamification.entities.RewardEntity;
 import org.springframework.data.repository.CrudRepository;
 
 public interface RewardRepository extends CrudRepository<RewardEntity, Long> {
+    //TODO implémenter getBadgesFromUser getScoresFromUser à partir des queries
 }

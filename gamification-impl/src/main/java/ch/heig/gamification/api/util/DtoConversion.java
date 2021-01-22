091f349091631dbc415f4fb8ee960747687e533b
Basile <basile.botebol@gmail.com>
2021-01-22 11:31:05 +0100

    repared get user stats

--- gamification-impl/src/main/java/ch/heig/gamification/api/util/DtoConversion.java
+++ gamification-impl/src/main/java/ch/heig/gamification/api/util/DtoConversion.java
@@ -1,8 +1,10 @@
 package ch.heig.gamification.api.util;
 
 import ch.heig.gamification.api.model.Badge;
-import ch.heig.gamification.entities.ApplicationEntity;
+import ch.heig.gamification.api.model.ScoreScale;
+
 import ch.heig.gamification.entities.BadgeEntity;
+import ch.heig.gamification.entities.ScoreScaleEntity;
 
 public final class DtoConversion {
 
@@ -11,10 +13,13 @@ public final class DtoConversion {
     public static Badge toBadge(BadgeEntity badgeEntity) {
         Badge badge = new Badge();
         badge.setName(badgeEntity.getName());
-        //badge.setApplicationEntity((ApplicationEntity)servletRequest.getAttribute("appEntity"));
         return badge;
     }
-
+    public static ScoreScale toScoreScale(ScoreScaleEntity entity) {
+        ScoreScale scoreScale = new ScoreScale();
+        scoreScale.setName(entity.getName());
+        return scoreScale;
+    }
 
 
 
